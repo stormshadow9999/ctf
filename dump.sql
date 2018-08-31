84bd5e9f0847b7d81b5e6c619b4bf9fa5cc6d6fa
amodsachintha <amod077@gmail.com>
2018-09-01 00:53:42 +0530

    create common database by name: whitewalker

--- dump.sql
+++ dump.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `emp`
 --
 
+CREATE DATABASE IF NOT EXISTS whitewalker;
+USE whitewalker;
+
+
 DROP TABLE IF EXISTS `emp`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
